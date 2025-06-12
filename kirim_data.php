206afd225c979dc5d8e744368d73c10248a3ee2a
Annasyatqia <55annasya@gmail.com>
2025-06-12 20:53:43 +0700

    Auto commit on save

--- kirim_data.php
+++ kirim_data.php
@@ -6,12 +6,18 @@ $db = "iot_db";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
-$gerakan = $_GET['gerakan'];
-$cahaya = $_GET['cahaya'];
+// Ambil data dari ESP8266
+$gerakan = $_GET['gerakan'] ?? 'NULL';
+$cahaya = $_GET['cahaya'] ?? 'NULL';
 $waktu = date("Y-m-d H:i:s");
 
+// Simpan ke database
 $sql = "INSERT INTO sensor_data (waktu, gerakan, cahaya) VALUES ('$waktu', '$gerakan', '$cahaya')";
 $conn->query($sql);
 
-echo "Data tersimpan";
+// Tampilkan data untuk debug
+echo "Data tersimpan:<br>";
+echo "Waktu: $waktu<br>";
+echo "Gerakan: $gerakan<br>";
+echo "Cahaya: $cahaya<br>";
 ?>
